a28940aa12fdc203c72d5974a66b0f5db56910f5
ObeeK <chasenharvey@gmail.com>
2021-08-24 13:35:44 -0500

    randomized first attack

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,8 +32,12 @@ var fightOrSkip = function () {
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemy) {
-  
+  var isPlayerTurn = true;
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
     while (playerInfo.health > 0 && enemy.health > 0) {
+      if (isPlayerTurn) {
       // ask player if they'd like to fight or run
       if (fightOrSkip()) {
         break;
@@ -60,6 +64,7 @@ var fight = function(enemy) {
       } else {
         window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
       }
+    } else {
   
       // remove players's health by subtracting the amount set in the enemy.attack variable
       var damage = randomNumber(enemy.attack - 3, enemy.attack);
@@ -77,6 +82,8 @@ var fight = function(enemy) {
         window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
       }
     }
+    isPlayerTurn = !isPlayerTurn;
+  }
   };
 
 // fight each enemy-robot by looping over them and fighting them one at a time
